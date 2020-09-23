d798be4b42d7d31c9d1010cde9a411fbb4a384f8
sbrick26 <swayambarik@gmail.com>
2020-09-23 01:31:29 -0500

    working logic to go through different questions

--- chatbot.py
+++ chatbot.py
@@ -1,16 +1,16 @@
 from random import choice
 
-questions_list = ["What is your favorite food?", "What language do you code in?", "Do you have any movies you would recommend?", "What's your favorite song?", "What is your favorite hobby?", "What streaming service do you use?", "What is your favorite restaurant?"]
+questions_list = ["What is your favorite food?", "What language do you code in?", "Do you have any movies you would recommend?", "What's your favorite song?", "What is your favorite hobby?", "What is your favorite streaming service to use?", "What is your favorite restaurant?"]
 
 positive_mood_list = ["Glad to hear! I am having a great day.", "That's great! I am feeling on top of the world!", "I am happy to hear! I am feeling quite exquisite myself."]
 
 negative_mood_list = ["Awww :( Your day will get better after talking to me!", "We all have some low days, let's talk to cheer you up!"]
 
-general_list = ["That's an interesting choice.", "Me too!!", "That sounds super intriguing.", "I have not tried it!"]
+general_list = ["That's an interesting choice.", "Me too!!", "That sounds super intriguing.", "I have not tried it!", "I would have never guessed that!", "That's a new one!", "I'm gonna go tell my Bot friends about this!"]
 
 why_list = ["I see, that makes sense!", "That's so cool!", "I get it now, I must try it myself!"]
 
-furthering_list = ["Why is that your choice?", "Tell me why you like it so much.", "Why did you choose that?", "Why do you like it?"]
+furthering_list = ["Why is that your choice?", "Tell me why you like it so much.", "Why did you choose that?", "Why do you like it?", "Tell me more about it!"]
 
 def get_bot_response(user_response, chatlevel):
     response = user_response.lower()
@@ -21,47 +21,55 @@ def get_bot_response(user_response, chatlevel):
             return choice(negative_mood_list)
         elif "well" in response or "good" in response or "amazing" in response or "great" in response or "happy" in response:
             return choice(positive_mood_list)
+        else:
+            return "Nice."
     elif chatlevel == 1:
-        answer = choice(general_list)
         if len(general_list) > 0:
+            answer = choice(general_list)
             general_list.remove(answer)
-            return answer
+            return "Pi'erre: " + answer
         else:
-            return "I have to go, have a nice day!"
+            return "Pi'erre: I have to go, have a nice day! Enter done to end chat:"
     elif chatlevel == 2:
-        answer = choice(why_list)
-        if len(why_list) > 0:
-            why_list.remove(answer)
-            return answer
-        else:
-            return "I have to go, have a nice day!"
+        answer = "Pi'erre: " + choice(why_list)
+        return answer
         
     
-
+name = "New User: "
 stillTalking = True
 chatlevel = 0
 while stillTalking:
     if chatlevel == 0:
-        print("Hello there! I'm Pi'erre, the chat bot! Let's chat.")
-        print("To exit this chat at any time, enter done")
-        user_response = input("How are you doing today?\n")
+        print("Pi'erre: Hello there! I'm Pi'erre, the chat bot! Let's chat.")
+        print("Pi'erre: To exit this chat at any time, enter done")
+        name = input ("Pi'erre: What is your name?\nUser: ") + ": "
+        user_response = input("Pi'erre: How are you doing today?\n" + name)
         if user_response == "done":
             break
-        print(get_bot_response(user_response, chatlevel))
+        print("Pi'erre: " + get_bot_response(user_response, chatlevel))
         chatlevel = 1
         
     elif chatlevel == 1:
-        user_response = input(choice(questions_list)+ "\n")
+        
+
+        if len(questions_list) > 0:
+            answer = choice(questions_list)
+            questions_list.remove(answer)
+        else:
+            print("Pi'erre: I have to go, have a nice day!")
+            break
+        user_response = input("Pi'erre: " + answer+ "\n" + name)
         if user_response == "done":
             break
-        print(get_bot_response(user_response, chatlevel))
+        print(get_bot_response("Pi'erre: " + user_response, chatlevel))
         chatlevel = 2
     
     elif chatlevel == 2:
-        user_response = input(choice(furthering_list) + "\n")
+        answer = choice(furthering_list)
+        user_response = input("Pi'erre: " + answer + "\n" + name)
         if user_response == "done":
             break
-        print(get_bot_response(user_response, chatlevel))
+        print(get_bot_response("Pi'erre: " + user_response, chatlevel))
         chatlevel = 1
     
 
